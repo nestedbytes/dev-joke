53fc1c61005052bb242b0ad8d11c95445df7581e
Shour <shourjjomajum@outlook.com>
2023-01-09 17:07:26 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,23 +1,20 @@
 import kivy
+import random
 from kivy.app import App
 from kivy.uix.label import Label
-import pyjokes
-  
 
-My_joke = pyjokes.get_joke(language="en", category="neutral")
-  
-
-  
-
-class MyApp(App):
-
+class JokeApp(App):
     def build(self):
-        self.title = 'Developer Joke'
-      
-        return Label(text=My_joke)
+        jokes = [
+          
+            "Why was the math book sad? Because it had too many problems.",
+            "Why did the edge server go bankrupt? Because it ran out of cache."
+            "Why did the Java developer teach his young kids about single quotes? Because they build character"
+            "Why do most Java programmers wear glasses? Because they don’t see sharp"
+            
+        ]
+        joke = random.choice(jokes)
+        return Label(text=joke)
 
-      
-
-
-if __name__ == '__main__':
-    MyApp().run()
+if __name__ == "__main__":
+    JokeApp().run()
